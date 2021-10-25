4740c96becf5de16a5f6ec8a396cda7fffca5e4d
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-25 18:01:08 +0200

    Assigning to the handler, a callback function that will log a clicked text in the console whenever the checkbox is clicked

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,8 +3,11 @@ import React from "react"
 function TodoItem(props) {
     return( 
       <li>
-       <input type="checkbox" checked={props.todo.completed} /> {props.todo.title}
-      </li>
+        <input type="checkbox"
+        checked={props.todo.completed}
+        onChange={() => console.log("clicked")}
+        /> {props.todo.title}
+        </li>
     );
   }
 
