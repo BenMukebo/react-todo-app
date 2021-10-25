8ed0cf0e1cc80e9e0321bd60c1a54dc822f7d512
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-25 13:48:02 +0200

    Import TodoContainer component inside the index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TodoContainer from "./components/TodoContainer"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
