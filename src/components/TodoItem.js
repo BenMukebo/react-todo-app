0dc644c012b5c3550b367501a716d0185164b15f
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-25 17:49:57 +0200

    Add a checked prop to the input checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 function TodoItem(props) {
-    return <li><input type="checkbox" /> {props.todo.title}</li>
+    return( 
+      <li>
+       <input type="checkbox" checked={props.todo.completed} /> {props.todo.title}
+      </li>
+    );
   }
 
 export default TodoItem
