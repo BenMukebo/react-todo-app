1c29422f27fa97335d5111c55b166afbfe4d7b73
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-26 13:50:37 +0200

    Creat the InputTodo's state

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react"
 
 class InputTodo extends Component {
+  state = {
+    title: ""
+  };
+
   render() {
     return (
       <form>
-        <input type="text" placeholder="Add Todo..." />
+        <input type="text" placeholder="Add todo..." value={this.state.title} />
         <button>Submit</button>
       </form>
     )
