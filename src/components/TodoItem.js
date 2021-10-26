e57f698cfbf600b90e381b6888c73afd35a8ef60
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-26 18:49:10 +0200

    Update the markup within the return statement of TodoItem.js to include the class names

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,18 +2,20 @@ import React from "react"
 import styles from "./TodoItem.module.css"
 
 function TodoItem(props) {
-    return( 
-      <li>
-        <input type="checkbox"
+  return (
+    <li className={styles.item}>
+      <input
+        type="checkbox"
+        className={styles.checkbox}
         checked={props.todo.completed}
         onChange={() => props.handleChangeProps(props.todo.id)}
-        />
-        <button onClick={() => props.deleteTodoProps(props.todo.id)}>
-          Delete 
-        </button>
-        {props.todo.title}
-        </li>
-    );
-  }
+      />
+      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
+        Delete
+      </button>
+      {props.todo.title}
+    </li>
+  );
+}
 
 export default TodoItem
