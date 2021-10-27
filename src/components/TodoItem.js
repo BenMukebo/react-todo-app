06b75d37ae174211b713c593c646e705da1e1771
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-27 12:15:19 +0200

    last

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,36 +1,48 @@
-import React from "react"
+// import React from "react"
+import { Component } from "react"
 import styles from "./TodoItem.module.css"
 
-function TodoItem(props) {
-  const completedStyle = {
-    fontStyle: "italic",
-    color: "#595959",
-    opacity: 0.4,
-    textDecoration: "line-through",
-  }
+const completedStyle = {
+  fontStyle: "italic",
+  color: "#595959",
+  opacity: 0.4,
+  textDecoration: "line-through",
+}
 
-  const handleEditing = () => {
+class TodoItem extends Component{
+  constructor(props) {
+    super(props)
+    // const { completed, id, title } = this.props.todo;
+    this.state = {
+      editing: false,
+      completed: props.todo.completed,
+      id: props.todo.id,
+      title: props.todo.title
+    }
+
+  }
+  handleEditing = () => {
     console.log("edit mode activated")
   }
+  
 
-  const { completed, id, title } = props.todo
-
-  return (
-    <li className={styles.item}>
-      <div onDoubleClick={ handleEditing }>
-        <input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => props.handleChangeProps(id)}
-        />
-        <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>{title}</span>
-      </div>
-    </li>
-  )
-
-
+  render() {
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing }>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={this.state.completed}
+            onChange={() => this.props.handleChangeProps(this.state.id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(this.state.id)}>Delete</button>
+          <span style={this.state.completed ? completedStyle : null}>{this.state.title}</span>
+        </div>
+        {/* <input type="text" className={styles.textInput} /> */}
+      </li>
+    )
+  }
 }
 
 export default TodoItem
