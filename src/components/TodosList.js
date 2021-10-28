20a44d8e53595b788b3089a54cec4ac70faac48f
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-28 11:19:52 +0200

    Add Constructor method and transform the classes in stateless function

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,21 +1,35 @@
 import React from "react"
 import TodoItem from "./TodoItem";
 
-class TodosList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map(todo => (
-        //   <li key={todo.id}>{todo.title}</li>
+// class TodosList extends React.Component {
+//   render() {
+//     return (
+//       <ul>
+//         {this.props.todos.map(todo => (
+//         //   <li key={todo.id}>{todo.title}</li>
+//         <TodoItem key={todo.id} todo={todo}
+//          handleChangeProps={this.props.handleChangeProps} 
+//          deleteTodoProps={this.props.deleteTodoProps} 
+//          setUpdate={this.props.setUpdate}
+//          />
+//         ))}
+//       </ul>
+//     )
+//   }
+// }
+
+const TodosList = (props) => {
+  return ( 
+    <ul>
+        {props.todos.map(todo => (
         <TodoItem key={todo.id} todo={todo}
-         handleChangeProps={this.props.handleChangeProps} 
-         deleteTodoProps={this.props.deleteTodoProps} 
-         setUpdate={this.props.setUpdate}
+         handleChangeProps={props.handleChangeProps} 
+         deleteTodoProps={props.deleteTodoProps} 
+         setUpdate={props.setUpdate}
          />
         ))}
       </ul>
-    )
-  }
+   );
 }
-
-export default TodosList
+ 
+export default TodosList;
